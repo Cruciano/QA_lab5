3af6a017929f428e87a4194c218b343031be2113
Maksym <43701413+Cruciano@users.noreply.github.com>
2021-12-14 10:43:22 +0200

    Add lab6

---
+++ playwright.config.js
@@ -0,0 +1,8 @@
+const config = {
+	retries: 1,
+	use: {
+		trace: 'retain-on-failure',
+	},
+};
+
+module.exports = config;
--- test.mjs
+++ test.mjs
@@ -1,73 +1,91 @@
 import { test, expect } from '@playwright/test';
 
+class SignUpPage {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async Navigate() {
+    await this.page.goto('https://www.demoblaze.com/');
+  }
+
+  async SignUp() {
+    await this.page.click('a:has-text("Sign up")');
+    await this.page.click('text=Username: Password: >> input[type="text"]');
+    await this.page.fill('text=Username: Password: >> input[type="text"]', '13451345');
+    await this.page.click('input[type="password"]');
+    await this.page.fill('input[type="password"]', '11111');
+    this.page.once('dialog', dialog => {
+      console.log(`Dialog message: ${dialog.message()}`);
+      dialog.dismiss().catch(() => {});
+    });
+    await this.page.click('button:has-text("Sign up")');
+  }
+}
+
+class LogInPage {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async Navigate() {
+    await this.page.goto('https://www.demoblaze.com/');
+  }
+
+  async LogIn() {
+    await this.page.click('a:has-text("Log in")');
+    await this.page.click('text=Log in × Username: Password: Close Log in >> input[type="text"]');
+    await this.page.fill('text=Log in × Username: Password: Close Log in >> input[type="text"]', '13451345');
+    await this.page.click('text=Log in × Username: Password: Close Log in >> input[type="password"]');
+    await this.page.fill('text=Log in × Username: Password: Close Log in >> input[type="password"]', '11111');
+    await Promise.all([
+      this.page.waitForNavigation(/*{ url: 'https://www.demoblaze.com/' }*/),
+      this.page.click('button:has-text("Log in")')
+    ]);
+    }
+}
+
+class CartPage {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async Navigate() {
+    await this.page.goto('https://www.demoblaze.com/');
+  }
+
+  async AddToCart() {
+    await this.page.click('text=Iphone 6 32gb');
+    await expect(this.page).toHaveURL('https://www.demoblaze.com/prod.html?idp_=5');
+    this.page.once('dialog', dialog => {
+      console.log(`Dialog message: ${dialog.message()}`);
+      dialog.dismiss().catch(() => {});
+    });
+    await this.page.click('text=Add to cart');
+    await expect(this.page).toHaveURL('https://www.demoblaze.com/prod.html?idp_=5#');
+  }
+
+  async DeleteFromCart() {
+    await this.page.click('text=Cart');
+    await expect(this.page).toHaveURL('https://www.demoblaze.com/cart.html');
+    await Promise.all([
+      this.page.waitForNavigation(/*{ url: 'https://www.demoblaze.com/cart.html#' }*/),
+      this.page.click('text=Delete')
+    ]);
+  }
+}
+
 test('test', async ({ page }) => {
+  const signUpPage = new SignUpPage(page);
+  await signUpPage.Navigate();
+  await signUpPage.SignUp();
 
-  // Go to https://www.demoblaze.com/
-  await page.goto('https://www.demoblaze.com/');
+  const logInPage = new LogInPage(page);
+  await logInPage.Navigate();
+  logInPage.LogIn();
 
-  // Click a:has-text("Sign up")
-  await page.click('a:has-text("Sign up")');
-
-  // Click text=Username: Password: >> input[type="text"]
-  await page.click('text=Username: Password: >> input[type="text"]');
-
-  // Fill text=Username: Password: >> input[type="text"]
-  await page.fill('text=Username: Password: >> input[type="text"]', '13451345');
-
-  // Click input[type="password"]
-  await page.click('input[type="password"]');
-
-  // Fill input[type="password"]
-  await page.fill('input[type="password"]', '11111');
-
-  // Click button:has-text("Sign up")
-  page.once('dialog', dialog => {
-    console.log(`Dialog message: ${dialog.message()}`);
-    dialog.dismiss().catch(() => {});
-  });
-  await page.click('button:has-text("Sign up")');
-
-  // Click a:has-text("Log in")
-  await page.click('a:has-text("Log in")');
-
-  // Click text=Log in × Username: Password: Close Log in >> input[type="text"]
-  await page.click('text=Log in × Username: Password: Close Log in >> input[type="text"]');
-
-  // Fill text=Log in × Username: Password: Close Log in >> input[type="text"]
-  await page.fill('text=Log in × Username: Password: Close Log in >> input[type="text"]', '13451345');
-
-  // Click text=Log in × Username: Password: Close Log in >> input[type="password"]
-  await page.click('text=Log in × Username: Password: Close Log in >> input[type="password"]');
-
-  // Fill text=Log in × Username: Password: Close Log in >> input[type="password"]
-  await page.fill('text=Log in × Username: Password: Close Log in >> input[type="password"]', '11111');
-
-  // Click button:has-text("Log in")
-  await Promise.all([
-    page.waitForNavigation(/*{ url: 'https://www.demoblaze.com/' }*/),
-    page.click('button:has-text("Log in")')
-  ]);
-
-  // Click text=Iphone 6 32gb
-  await page.click('text=Iphone 6 32gb');
-  await expect(page).toHaveURL('https://www.demoblaze.com/prod.html?idp_=5');
-
-  // Click text=Add to cart
-  page.once('dialog', dialog => {
-    console.log(`Dialog message: ${dialog.message()}`);
-    dialog.dismiss().catch(() => {});
-  });
-  await page.click('text=Add to cart');
-  await expect(page).toHaveURL('https://www.demoblaze.com/prod.html?idp_=5#');
-
-  // Click text=Cart
-  await page.click('text=Cart');
-  await expect(page).toHaveURL('https://www.demoblaze.com/cart.html');
-
-  // Click text=Delete
-  await Promise.all([
-    page.waitForNavigation(/*{ url: 'https://www.demoblaze.com/cart.html#' }*/),
-    page.click('text=Delete')
-  ]);
-
+  const cartPage = new CartPage(page);
+  cartPage.Navigate();
+  cartPage.AddToCart();
+  cartPage.DeleteFromCart();
 });
